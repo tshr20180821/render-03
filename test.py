d6fcaf3e0205525c5eb1f58fb2be94bc4b47a11c
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-02-20 05:46:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print('test')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/test01')
+def test01():
+    test01 = "test01"
+    return test01
+
+if __name__ == "__main__":
+    app.run()
